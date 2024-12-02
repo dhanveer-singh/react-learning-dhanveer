865b5fcf29a6cc6b9f0dbfc5f2c67d0cf32412c1
dhanveer-singh <dhanveersingh@zapbuild.com>
2024-12-02 10:34:46 +0530

    input counter

--- src/components/profileDetails.jsx
+++ src/components/profileDetails.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 
 const ProfileDetails = ({ userData }) => {
     const { name, age, location } = userData;
+    const [count, setCount] = useState(0);
     return (
         <>
             <h1>User Details</h1>
@@ -9,6 +11,11 @@ const ProfileDetails = ({ userData }) => {
                 <p>Age: {age}</p>
                 <p>Location: {location}</p>
             </div>
+
+            <h1>Counter</h1>
+            <div>{count}</div>
+            <button onClick={() => setCount(prev => prev - 1)} disabled={count <= 0}>-</button>
+            <button onClick={() => setCount(prev => prev + 1)}>+</button>
         </>
     )
 }
