11f24a19d2260eed93f383fd8b590a7ca7ecec96
dhanveer-singh <dhanveersingh@zapbuild.com>
2024-12-06 13:32:12 +0530

    Updated project setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
-
 
 import router from './routes/AppRoutes';
 
